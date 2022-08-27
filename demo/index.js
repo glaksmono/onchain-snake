7318ac361f846cc738fa3591883a41a4ed9b7d6b
glaksmono <grady@laksmono.com>
2022-08-27 14:47:32 +0700

    init onchain snake

---
+++ demo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SnakeGame from '../src/SnakeGame.jsx'
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>onchain-snake</h1>
+    <div className="textWrapper">
+      <p id="instructions">Use the arrow keys or W/A/S/D to play</p>
+    </div>
+    <SnakeGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
