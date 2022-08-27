6e58411494ea65ee2b2bf168217917d5dc87944a
glaksmono <grady@laksmono.com>
2022-08-27 16:21:37 +0700

    make some updates

--- webpack.config.js
+++ webpack.config.js
@@ -6,22 +6,22 @@ module.exports = {
     target: 'node',
     externals: [nodeExternals()],
     entry: path.join(__dirname, 'src', 'SnakeGame.jsx'),
-	output: {
+    output: {
         path: path.resolve('lib'),
         filename: 'snake.js',
         libraryTarget: 'commonjs2',
     },
-	module: {
-		rules: [
-			{
+    module: {
+        rules: [
+            {
                 test: /\.(js|jsx)?$/,
                 exclude: /(node_modules)/,
                 use: "babel-loader"
-			},
-			{
+            },
+            {
                 test: /\.css/,
                 use: ['style-loader', 'css-loader']
-			},
-		]
+            },
+        ]
     },
 }
